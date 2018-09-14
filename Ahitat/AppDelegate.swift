6c9911859460886b5dbc8bd8a5f2f767ef7fa665
Daniel Szasz <daniel.szasz@mobiversal.com>
2018-09-14 23:48:44 +0300

    splash, assets and hamburger menu added

--- Ahitat/AppDelegate.swift
+++ Ahitat/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         setWindow()
@@ -22,8 +21,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     private func setWindow() {
         window = UIWindow(frame: UIScreen.main.bounds)
-        let initialVc = UIViewController()
-        let navController = UINavigationController(rootViewController: initialVc)
+        let initialVc = MainViewController()
+        let navController = UINavigationController()
+        navController.setViewControllers([initialVc], animated: true)
         window?.rootViewController = navController
         window?.makeKeyAndVisible()
     }
---
+++ Ahitat/MainViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MainViewController.swift
+//  Ahitat
+//
+//  Created by Daniel Szasz on 9/14/18.
+//  Copyright © 2018 Daniel Szasz. All rights reserved.
+//
+
+import UIKit
+import SideMenu
+
+class MainViewController: UIViewController {
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureSideMenu()
+        addNavBarButtons()
+
+        self.navigationController?.navigationBar.setBackgroundImage(#imageLiteral(resourceName: "bgHeader"), for: .topAttached, barMetrics: .default)
+        self.navigationController?.navigationBar.tintColor = .white
+    }
+
+    private func configureSideMenu() {
+        // Define the menus
+        let menuLeftNavigationController = UISideMenuNavigationController(rootViewController: MenuViewController())
+        SideMenuManager.default.menuLeftNavigationController = menuLeftNavigationController
+    }
+
+    private func addNavBarButtons() {
+
+        let hamburger = UIBarButtonItem(image: #imageLiteral(resourceName: "iconHamburgerMenu"), style: .plain, target: self, action: #selector(openSideMenu))
+        self.navigationItem.leftBarButtonItems = [hamburger]
+
+        let calendar = UIBarButtonItem(image: #imageLiteral(resourceName: "iconCalendar"), style: .plain, target: self, action: #selector(toggleCalendar))
+        self.navigationItem.rightBarButtonItems = [calendar]
+    }
+
+    @objc private func openSideMenu() {
+        present(SideMenuManager.default.menuLeftNavigationController!, animated: true, completion: nil)
+//        dismiss(animated: true, completion: nil)
+    }
+
+    @objc private func toggleCalendar() {
+
+    }
+}

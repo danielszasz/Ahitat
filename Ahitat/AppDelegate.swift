577f2f61aee0b27060a45f24719bb578d8dd2d46
Daniel Szasz <daniel.szasz@mobiversal.com>
2018-09-14 22:48:05 +0300

    added assets and configured window

--- Ahitat/AppDelegate.swift
+++ Ahitat/AppDelegate.swift
@@ -15,8 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        setWindow()
         return true
+    }
+
+    private func setWindow() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let initialVc = UIViewController()
+        let navController = UINavigationController(rootViewController: initialVc)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
     }
 
     func applicationWillResignActive(_ application: UIApplication) {

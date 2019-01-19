f02679d03f944c78c46f037a35b66ab5ba7b6a93
Daniel Szasz <daniel.szasz@mobiversal.com>
2019-01-19 18:43:43 +0200

    bug fix

--- Ahitat/MainViewController.swift
+++ Ahitat/MainViewController.swift
@@ -27,7 +27,6 @@ class MainViewController: UIViewController {
             guard let med = currentMeditation else {return}
             calendarView.select(date: med.date)
             setMeditationViews(with: med)
-            scrollView.scrollsToTop
         }
     }
 

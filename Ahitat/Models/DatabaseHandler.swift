bd1b8c911656784d5aec4a3f7cef0a7af942596e
Daniel Szasz <daniel1.szasz@btrl.ro>
2024-01-18 13:34:56 +0200

    database update

--- Ahitat/Models/DatabaseHandler.swift
+++ Ahitat/Models/DatabaseHandler.swift
@@ -11,7 +11,7 @@ import Foundation
 
 final class DatabaseHandler {
     private var database: Connection? {
-        let path = Bundle.main.path(forResource: "devotional2023", ofType: "sqlite") ?? ""
+        let path = Bundle.main.path(forResource: "devotional2024", ofType: "sqlite") ?? ""
         return try? Connection(path, readonly: true)
     }
 
